f7836e9b24b210fa00df589b33d3ba5c727803dc
SakinaBamba <70284590+SakinaBamba@users.noreply.github.com>
2025-05-10 22:48:05 -0400

    Update startTranscription.ts

--- pages/api/teams/startTranscription.ts
+++ pages/api/teams/startTranscription.ts
@@ -4,6 +4,7 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import { Client } from '@microsoft/microsoft-graph-client';
 import 'isomorphic-fetch';
 
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'POST') return res.status(405).end('Method Not Allowed');
 
